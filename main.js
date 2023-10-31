921853ceb554f4aae72d786483d8a1a806192add
sigrunedda <sem24@hi.is>
2023-10-31 16:36:54 +0000

    tengja á github

---
+++ main.js
@@ -0,0 +1,73 @@
+import { createCartLine, showCartContent } from './lib/ui.js';
+
+const products = [
+  {
+    id: 1,
+    title: 'HTML húfa',
+    description:
+      'Húfa sem heldur hausnum heitum og hvíslar hugsanlega að þér hvaða element væri best að nota.',
+    price: 5_000,
+  },
+  {
+    id: 2,
+    title: 'CSS sokkar',
+    description: 'Sokkar sem skalast vel með hvaða fótum sem er.',
+    price: 3_000,
+  },
+  {
+    id: 3,
+    title: 'JavaScript jakki',
+    description: 'Mjög töff jakki fyrir öll sem skrifa JavaScript reglulega.',
+    price: 20_000,
+  },
+];
+
+/** Bæta vöru í körfu */
+function addProductToCart(product, quantity) {
+  // Hér þarf að finna `<tbody>` í töflu og setja `cartLine` inn í það
+ // const cart = document.querySelector('.cart');
+  const cartTable = document.querySelector('.cart-table');
+  const cartTableBody = document.querySelector('.cart-table tbody');
+
+  
+  // TODO hér þarf að athuga hvort lína fyrir vöruna sé þegar til
+  const cartLine = createCartLine(product, quantity);
+  cartTableBody.appendChild(cartLine);
+
+  // Sýna efni körfu
+  showCartContent(true);
+
+  // TODO sýna/uppfæra samtölu körfu
+}
+
+function submitHandler(event) {
+  // Komum í veg fyrir að form submiti
+  event.preventDefault();
+  
+  // Finnum næsta element sem er `<tr>`
+  const parent = event.target.closest('tr')
+
+  // Það er með attribute sem tiltekur auðkenni vöru, t.d. `data-product-id="1"`
+  const productId = Number.parseInt(parent.dataset.productId);
+
+  // Finnum vöru með þessu productId
+  const product = products.find((i) => i.id === productId);
+
+  // TODO hér þarf að finna fjölda sem á að bæta við körfu með því að athuga
+  // á input
+  const quantity = 1;
+
+  // Bætum vöru í körfu (hér væri gott að bæta við athugun á því að varan sé til)
+  addProductToCart(product, quantity);
+}
+
+// Finna öll form með class="add"
+const addToCartForms = document.querySelectorAll('.add')
+
+// Ítra í gegnum þau sem fylki (`querySelectorAll` skilar NodeList)
+for (const form of Array.from(addToCartForms)) {
+  // Bæta submit event listener við hvert
+  form.addEventListener('submit', submitHandler);
+}
+
+// TODO bæta við event handler á form sem submittar pöntun
